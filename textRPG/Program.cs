5c6a3e6b64f70be67225e6b7300ca68bf07a08f3
kjg5370 <kjk03024@naver.com>
2023-08-21 19:17:12 +0900

    상점 판매 구현
    상점에 아이템 판매 기능을 추가
    판매 시 구매가격의 85% 가격에 판매
    판매시 장착하고 있는 아이템이었다면 해제

--- textRPG/Program.cs
+++ textRPG/Program.cs
@@ -4,9 +4,9 @@ namespace textRPG
 {
     internal class Program
     {
-        private static Character player;
-        private static Equipment[] equipments = new Equipment[4];
-        private static ShopItem[] shopItems = new ShopItem[6];
+        private static Character player; 
+        private static List<Equipment> equipments = new List<Equipment>();
+        private static ShopItem[] shopItems = new ShopItem[8];
 
         static void Main(string[] args)
         {
@@ -20,19 +20,25 @@ namespace textRPG
             player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
 
             //인벤토리 아이템 정보 세팅
-            equipments[0] = new Equipment("무쇠갑옷", "방어력 +5", "무쇠로 만들어져 튼튼한 갑옷입니다.", ItemType.Armor, 5);
-            equipments[1] = new Equipment("낡은 검", "공격력 +2", "쉽게 볼 수 있는 낡은 검 입니다.", ItemType.Weapon, 2);
-            equipments[2] = new Equipment("거북이 등껍질", "방어력 +10", "무천도사가 늘 등에 짊어지고 있는 등껍질입니다.", ItemType.Armor, 10);
-            equipments[3] = new Equipment("여의봉", "공격력 +10", "길이는 조절할 수 있지만 굵기는 일정한 봉입니다.", ItemType.Weapon, 10);
+            equipments.Add(new Equipment("무쇠갑옷", "방어력 +9", "무쇠로 만들어져 튼튼한 갑옷입니다.", ItemType.Armor, 9));
+            equipments.Add(new Equipment("낡은 검", "공격력 +2", "쉽게 볼 수 있는 낡은 검 입니다.", ItemType.Weapon, 2));
+            equipments.Add(new Equipment("거북이 등껍질", "방어력 +10", "무천도사가 늘 등에 짊어지고 있는 등껍질입니다.", ItemType.Armor, 10));
+            equipments.Add(new Equipment("여의봉", "공격력 +10", "길이는 조절할 수 있지만 굵기는 일정한 봉입니다.", ItemType.Weapon, 10));
 
             //상점 아이템 정보 세팅
             shopItems[0] = new ShopItem("수련자 갑옷", "방어력 +5", "수련에 도움을 주는 갑옷입니다.", ItemType.Armor, 5, 1000);
-            shopItems[1] = new ShopItem("무쇠갑옷", "방어력 +9", "무쇠로 만들어져 튼튼한 갑옷입니다.", ItemType.Armor, 9, 0);
+            shopItems[1] = new ShopItem("무쇠갑옷", "방어력 +9", "무쇠로 만들어져 튼튼한 갑옷입니다.", ItemType.Armor, 9, 2118);
             shopItems[2] = new ShopItem("스파르타의 갑옷", "방어력 +15", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", ItemType.Armor,15, 3500);
-            shopItems[3] = new ShopItem("낡은 검", "공격력 +2", "쉽게 볼 수 있는 낡은 검 입니다.", ItemType.Weapon, 2, 0);
+            shopItems[3] = new ShopItem("낡은 검", "공격력 +2", "쉽게 볼 수 있는 낡은 검 입니다.", ItemType.Weapon, 2, 600);
             shopItems[4] = new ShopItem("청동 도끼", "공격력 +5", "어디선가 사용됐던거 같은 도끼입니다.", ItemType.Weapon, 5, 1500);
             shopItems[5] = new ShopItem("스파르타의 창", "공격력 +7", "스파르타의 전사들이 사용했다는 전설의 창입니다.", ItemType.Weapon, 7, 3500);
+            shopItems[6] = new ShopItem("거북이 등껍질", "방어력 +10", "무천도사가 늘 등에 짊어지고 있는 등껍질입니다.", ItemType.Armor, 10,2000);
+            shopItems[7] = new ShopItem("여의봉", "공격력 +10", "길이는 조절할 수 있지만 굵기는 일정한 봉입니다.", ItemType.Weapon, 10,2000);
 
+            shopItems[1].Price = 0;
+            shopItems[3].Price = 0;
+            shopItems[6].Price = 0;
+            shopItems[7].Price = 0;
         }
 
         static void DisplayGameIntro()
@@ -164,7 +170,7 @@ namespace textRPG
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
-            int input = CheckValidInput(0, equipments.Length);
+            int input = CheckValidInput(0, equipments.Count);
             if (input == 0)
             {
                 DisplayInventory();
@@ -180,7 +186,7 @@ namespace textRPG
             Console.Clear();
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("상점- 아이템 구매");
+            Console.WriteLine("상점");
             Console.ResetColor();
             Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
             Console.WriteLine();
@@ -192,16 +198,128 @@ namespace textRPG
             foreach (ShopItem shopitem in shopItems)
             {
                 index++;
-                shopitem.PrintFormatted(true, index);
+                shopitem.PrintFormatted();
+            }
+            Console.WriteLine();
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("2. 아이템 판매");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            int input = CheckValidInput(0, 2);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+
+                case 1:
+                    DisplayShopBuy();
+                    break;
+                case 2:
+                    DisplayShopSell();
+                    break;
+            }
+        }
+        static void DisplayShopBuy()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("상점 - 아이템 구매");
+            Console.ResetColor();
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            Console.WriteLine();
+            Console.WriteLine($"[보유 골드]");
+            Console.WriteLine($"{player.Gold} G");
+            Console.WriteLine();
+            Console.WriteLine($"[아이템 목록]");
+            int index = 0;
+            foreach (ShopItem shopitem in shopItems)
+            {
+                index++;
+                shopitem.PrintFormatted(true,index);
             }
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
-            int input = CheckValidInput(0, equipments.Length);
+            int input = CheckValidInput(0, shopItems.Length);
             if (input == 0)
             {
-                DisplayGameIntro();
+                DisplayShop();
+            }
+            else
+            {
+                BuyItem(shopItems[input -1]) ;
+            }
+        }
+        static void BuyItem(ShopItem item)
+        {
+            if (item.Price == 0)
+            {
+                Console.WriteLine("이미 구매한 아이템입니다.");
+            }
+            else if (player.Gold >= item.Price)
+            {
+                Equipment newEquipment = new Equipment(item.eName, item.Stat, item.Data, item.Type, item.StatValue);
+                player.Gold -= item.Price;
+                item.Price = 0; // 구매 완료 표시
+                equipments.Add(newEquipment);
+                Console.WriteLine($"{item.eName}을(를) 구매했습니다. 남은 골드: {player.Gold} G");
+            }
+            else
+            {
+                Console.WriteLine("Gold 가 부족합니다.");
+            }
+
+            Console.WriteLine();
+            DisplayShopBuy();
+        }
+        static void DisplayShopSell()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("상점 - 아이템 판매");
+            Console.ResetColor();
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            Console.WriteLine();
+            Console.WriteLine($"[보유 골드]");
+            Console.WriteLine($"{player.Gold} G");
+            Console.WriteLine();
+            Console.WriteLine($"[아이템 목록]");
+            int index = 0;
+            List<ShopItem> matchingShopItems = new List<ShopItem>();
+
+            foreach (Equipment equipment in equipments)
+            {
+                ShopItem matchingShopItem = shopItems.FirstOrDefault(shopItem => equipment.eName == shopItem.eName);
+                if (matchingShopItem != null)
+                {
+                    ++index;
+                    matchingShopItems.Add(matchingShopItem);
+                    matchingShopItems[index - 1].Sell();
+                    matchingShopItems[index - 1].PrintFormatted(true, index);
+                    matchingShopItems[index - 1].Price = 0;
+
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            int input = CheckValidInput(0, matchingShopItems.Count);
+            if (input == 0)
+            {
+                DisplayShop();
+            }
+            else
+            {
+                equipments[input - 1].SellingItem(player, matchingShopItems[input-1]);
+                equipments.Remove(equipments[input - 1]);
+                matchingShopItems[input - 1].Price = matchingShopItems[input - 1].basePrice;
+                DisplayShopSell();
             }
         }
     }
@@ -219,7 +337,7 @@ namespace textRPG
         public int Atk { get; set; }
         public int Def { get; set; }
         public int Hp { get; }
-        public int Gold { get; }
+        public int Gold { get; set; }
 
         public Character(string name, string job, int level, int atk, int def, int hp, int gold)
         {
@@ -249,7 +367,7 @@ namespace textRPG
         public string Data { get; private set; }
         public ItemType Type { get; }
         public int StatValue { get; }
-        string Mount = "[E]";
+        string Mount = "";
         public Equipment(string name, string stat, string data, ItemType type, int statValue)
         {
             eName = name;
@@ -261,14 +379,15 @@ namespace textRPG
         public virtual void PrintFormatted(bool useIndex = false, int index = -1)
         {
             string indexString = useIndex ? $"{index} " : "";
-            string formattedString = $"- {indexString}{eName,-10} |{Stat,-10} |{Data,-30}";
+            string formattedString = $"- {indexString}{Mount}{eName,-10} |{Stat,-10} |{Data,-30}";
             Console.WriteLine(formattedString);
         }
+        
         public void MountEquipment(Character player)
         {
-            if (eName.IndexOf(Mount) == -1)
+            if (Mount == "")
             {
-                eName = Mount + eName;
+                Mount = "[E]";
                 if (Type == ItemType.Weapon)
                 {
                     player.Atk += StatValue;
@@ -280,33 +399,50 @@ namespace textRPG
             }
             else
             {
-                eName = eName.Replace(Mount, "");
-                if (Type == ItemType.Weapon)
-                {
-                    player.Atk -= StatValue;
-                }
-                else if (Type == ItemType.Armor)
-                {
-                    player.Def -= StatValue;
-                }
+                UnmountEquipment(player);
             }
+        }
+
+        public void UnmountEquipment(Character player)
+        {
+            Mount = "";
+            if (Type == ItemType.Weapon)
+            {
+                player.Atk -= StatValue;
+            }
+            else if (Type == ItemType.Armor)
+            {
+                player.Def -= StatValue;
+            }
+        }
+        public void SellingItem(Character player, ShopItem item)
+        {
+            if (Mount == "[E]")
+                UnmountEquipment(player); // 장착 해제
+            player.Gold += (int)(item.basePrice * 0.85);
         }
     }
     public class ShopItem : Equipment
     {
-        public int Price { get; }
-
+        public int Price { get; set; }
+        public int basePrice;
         public ShopItem(string name, string stat, string description, ItemType type, int statValue, int price)
          : base(name, stat, description, type, statValue)
         {
             Price = price;
+            basePrice = Price;
         }
+        public void Sell()
+        {
+            Price += (int)(basePrice * 0.85);
+        }
+        
         public override void  PrintFormatted(bool useIndex = false, int index = -1)
         {
             string indexString = useIndex ? $"{index} " : "";
             string formattedString;
-            if (Price != 0)formattedString = $"- {indexString}{eName,-10} |{Stat,-10} |{Data,-30}|{Price}G";
-            else formattedString = $"- {indexString}{eName,-10} |{Stat,-10} |{Data,-30}|구매완료";
+            string priceInfo = Price == 0 ? "구매완료" : $"{Price} G";
+            formattedString = $"- {indexString}{eName,-10} |{Stat,-10} |{Data,-30}|{priceInfo,-10}";
             Console.WriteLine(formattedString);
         }
     }
